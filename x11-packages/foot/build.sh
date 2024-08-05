9ebf260cea5e60eb3fd5cdb19dd760aabc3bda5f
Shogo Yamazaki <pgp@mocknen.net>
2024-08-06 02:10:15 +0900

    fix(x11/foot): Fix memfd_create

--- x11-packages/foot/build.sh
+++ x11-packages/foot/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Fast, lightweight and minimalistic Wayland terminal emul
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.18.0"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://codeberg.org/dnkl/foot/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9d9f0efe4bca0bbf201482d6e7bb946a12a4b164d2e73dae75a2f2404e1e85ff
 TERMUX_PKG_AUTO_UPDATE=true
